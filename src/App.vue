a8674af361f65f6c0e7dcb4962ba772178bcce30
John C. Kennedy <devjckennedy@gmail.com>
2020-04-30 22:35:24 +0900

    update app style

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,16 @@
 </template>
 
 <style>
+a:focus,
+input:focus,
+textarea:focus {
+  outline: none;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
